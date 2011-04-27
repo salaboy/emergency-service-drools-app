91e52ed49ab422489b52e8590769a87b5279c8f1
Esteban <esteban.aliverti@gmail.com>
2011-04-27 17:54:58 -0300

    -VehicleDispatchedMessage created

---
+++ emergency-service-model/src/main/java/com/wordpress/salaboy/model/messages/VehicleDispatchedMessage.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wordpress.salaboy.model.messages;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author esteban
+ */
+public class VehicleDispatchedMessage implements Serializable {
+    private Long callId;
+    private Long vehicleId;
+
+    public VehicleDispatchedMessage(Long callId, Long vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public Long getCallId() {
+        return callId;
+    }
+
+    public void setCallId(Long callId) {
+        this.callId = callId;
+    }
+    
+    public Long getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(Long vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+    
+     
+}
