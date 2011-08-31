4ec05c4868dd3452722d263d1c68554acda54c56
salaboy <salaboy@gmail.com>
2011-08-31 10:03:11 -0300

    commenting test

--- emergency-service-core/src/test/java/com/wordpress/salaboy/procedures/DefaultFireProcedureNoSmartTasksTest.java
+++ emergency-service-core/src/test/java/com/wordpress/salaboy/procedures/DefaultFireProcedureNoSmartTasksTest.java
@@ -7,58 +7,29 @@ package com.wordpress.salaboy.procedures;
 import com.wordpress.salaboy.grid.GridBaseTest;
 import org.junit.Test;
 
-
-//import java.io.IOException;
-//import java.util.Date;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
-//import org.drools.grid.SocketService;
-//import org.example.ws_ht.api.TTaskAbstract;
-//import org.example.ws_ht.api.wsdl.IllegalAccessFault;
-//import org.example.ws_ht.api.wsdl.IllegalArgumentFault;
-//import org.example.ws_ht.api.wsdl.IllegalStateFault;
-//import org.hornetq.api.core.HornetQException;
-//import org.junit.After;
-//import org.junit.AfterClass;
-//import org.junit.Before;
-//import org.junit.BeforeClass;
-//
-//import com.wordpress.salaboy.api.HumanTaskService;
-//import com.wordpress.salaboy.api.HumanTaskServiceFactory;
-//import com.wordpress.salaboy.conf.HumanTaskServiceConfiguration;
-//import com.wordpress.salaboy.grid.GridBaseTest;
-//import com.wordpress.salaboy.messaging.MessageServerSingleton;
-//import com.wordpress.salaboy.model.Call;
-//import com.wordpress.salaboy.model.Emergency;
-//import com.wordpress.salaboy.model.FireTruck;
-//import com.wordpress.salaboy.model.Hospital;
-//import com.wordpress.salaboy.model.Location;
-//import com.wordpress.salaboy.model.messages.VehicleHitsEmergencyMessage;
-//import com.wordpress.salaboy.model.serviceclient.DistributedPeristenceServerService;
-//import com.wordpress.salaboy.services.HumanTaskServerService;
-//import com.wordpress.salaboy.services.ProceduresMGMTService;
-//import com.wordpress.salaboy.smarttasks.jbpm5wrapper.conf.JBPM5HornetQHumanTaskClientConfiguration;
-//import com.wordpress.salaboy.tracking.ContextTrackingServiceImpl;
-//import junit.framework.Assert;
-//import org.junit.Test;
-
 /**
  *
  * @author esteban
  */
-public class DefaultFireProcedureTest extends GridBaseTest {
+public class DefaultFireProcedureNoSmartTasksTest extends GridBaseTest {
+
     @Test
-    public void testMe(){}
-//
-//    private HumanTaskService humanTaskServiceClient;
+    public void testMe() {
+    }
+//    private  Emergency emergency = null;
+//    private FireTruck fireTruck = null;
+//    private MessageConsumer consumer;
+//    private TaskClient client;
+//    
+//    private Call call = null;
+//   
 //    
 //
-//    public DefaultFireProcedureTest() {
+//    public DefaultFireProcedureNoSmartTasksTest() {
 //    }
 //
-//    @BeforeClass
+//  
+//     @BeforeClass
 //    public static void setUpClass() throws Exception {
 //        HumanTaskServerService.getInstance().initTaskServer();
 //    }
@@ -68,49 +39,46 @@ public class DefaultFireProcedureTest extends GridBaseTest {
 //
 //        HumanTaskServerService.getInstance().stopTaskServer();
 //    }
-//
-//    Emergency emergency = null;
-//    FireTruck fireTruck = null;
-//    Call call = null;
+//  
 //    
-//    @Before
+//   @Before
 //    public void setUp() throws Exception {
 //        emergency = new Emergency();
 //        String emergencyId = ContextTrackingServiceImpl.getInstance().newEmergency();
 //        emergency.setId(emergencyId);
-//        
-//        fireTruck = new FireTruck("FireTruck 1");
-//                
-//        call = new Call(1,2,new Date());
-//
+//        call = new Call(1, 2, new Date());
 //        String callId = ContextTrackingServiceImpl.getInstance().newCall();
 //        call.setId(callId);
 //        emergency.setCall(call);
-//        emergency.setLocation(new Location(1,2));
-//        emergency.setType(Emergency.EmergencyType.FIRE);
+//        emergency.setLocation(new Location(1, 2));
+//        emergency.setType(Emergency.EmergencyType.HEART_ATTACK);
 //        emergency.setNroOfPeople(1);
-//        
-//        
+//
+//        ContextTrackingServiceImpl.getInstance().attachEmergency(callId, emergencyId);
+//
 //        DistributedPeristenceServerService.getInstance().storeHospital(new Hospital("My Hospital", 12, 1));
 //        DistributedPeristenceServerService.getInstance().storeEmergency(emergency);
-//        DistributedPeristenceServerService.getInstance().storeVehicle(fireTruck);
+//        DistributedPeristenceServerService.getInstance().storeVehicle(new Ambulance("My Ambulance Test"));
+//
 //        MessageServerSingleton.getInstance().start();
+//
 //
 //        this.coreServicesMap = new HashMap();
 //        createRemoteNode();
 //
-//        HumanTaskServiceConfiguration taskClientConf = new HumanTaskServiceConfiguration();
+//        client = HumanTaskServerService.getInstance().initTaskClient();
+//        this.coreServicesMap = new HashMap();
 //
 //
-//        taskClientConf.addHumanTaskClientConfiguration("jBPM5-HT-Client", new JBPM5HornetQHumanTaskClientConfiguration("127.0.0.1", 5446));
 //
-//        humanTaskServiceClient = HumanTaskServiceFactory.newHumanTaskService(taskClientConf);
-//        humanTaskServiceClient.initializeService();
 //
 //    }
 //
 //    @After
 //    public void tearDown() throws Exception {
+//
+//
+//
 //        MessageServerSingleton.getInstance().stop();
 //        if (remoteN1 != null) {
 //            remoteN1.dispose();
@@ -118,7 +86,9 @@ public class DefaultFireProcedureTest extends GridBaseTest {
 //        if (grid1 != null) {
 //            grid1.get(SocketService.class).close();
 //        }
+//
 //    }
+//
 //
 //    @Test
 //    public void defaultHeartAttackSimpleTest() throws HornetQException, InterruptedException, IOException, ClassNotFoundException, IllegalArgumentFault, IllegalStateFault, IllegalAccessFault {
